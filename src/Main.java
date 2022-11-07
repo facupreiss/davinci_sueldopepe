d462831dacb325e320124da9ca22acab8236751a
Facundo Preiss <115724589+facupreiss@users.noreply.github.com>
2022-11-07 12:41:55 -0300

    feat: implements Cadete class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main (String[] args) {
         System.out.println("Sueldo de pepe");
+
+        Cadete pepe = new Cadete();
+
+        System.out.println(String.format("El sueldo total del empleado es %f",
+                pepe.calcularSueldoNeto()
+                ));
     }
 }
