00988c76de90cf164c22269afacb49571903a65b
Facundo Preiss <115724589+facupreiss@users.noreply.github.com>
2022-11-07 12:30:18 -0300

    feat: add employ interface

---
+++ src/EmpleadoI.java
@@ -0,0 +1,6 @@
+public interface EmpleadoI {
+    double calcularSueldoNeto();
+    double calcularBonoPorPresentismo(int diasAusente);
+    double calcularBonoPorResultado ();
+    double calcularSueldoTotal(int diasAusente);
+}
